5807d87cf6e1bcd7c63aca58d00b4fbba5d618e2
misai-my <poseidonmustdie@gmail.com>
2026-06-18 16:36:45 +0800

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -9,5 +9,5 @@ window.PORTAL_CONFIG = {
   SITE_URL: "https://misai-my.github.io/codm/",
   AUTH_CALLBACK_URL: "https://misai-my.github.io/codm/auth-callback.html",
   RULEBOOK_URL: "https://docs.google.com/document/d/1hpJPhy6sgMaQy_zUnnN32-BB2NjgAT9Soz7_4RZXt78",
-  REGISTRATION_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLScfhYYJk2NckttjyZHfSH4GRCMiBYT2jp4dw2z7VKuWB9KBWw/viewform?edit2=2_ABaOnueI47O1nQ0i9v7gQXXCgyARsdqv-CCfXSEtanddJZlCVbEIHBXNwN6SxzMlBQ"
+  REGISTRATION_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLScfhYYJk2NckttjyZHfSH4GRCMiBYT2jp4dw2z7VKuWB9KBWw/viewform"
 };
